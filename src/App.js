b0e5c5c71350d0d3a7a066ab606e0b3611262da3
Mike Dreiling <mdarts@gmail.com>
2018-03-04 20:51:23 -0700

    transitions in place

--- src/App.js
+++ src/App.js
@@ -6,31 +6,13 @@ class App extends Component {
   constructor(props) {
     super(props);
   }
-  
+
   render() {
     return (
       <div className="App">
-        <div className="wrapper">
-          <header className="App-header">
-            <h1 className="App-title">Mike Dreiling</h1>
-            <p>
-              Im just a dude looking to get right back into code.  But above all, I love
-              to create things.
-            </p>
-          </header>
-          <div className="logoBoxA">
-            <Link to="/a"><h2>A</h2></Link>
-          </div>
-          <div className="logoBoxB">
-            <Link to="/b"><h2>B</h2></Link>
-          </div>
-          <div className="logoBoxC">
-            <Link to="/c"><h2>C</h2></Link>
-          </div>
           <div>
             {this.props.children}
           </div>
-        </div>
       </div>
     );
   }
--- src/Routes.js
+++ src/Routes.js
@@ -1,20 +1,49 @@
 import React from 'react';
-import { Redirect, Route, Router } from 'react-router-dom';
+import { Switch, Route, Router, Link } from 'react-router-dom';
 import A from './A';
 import B from './B';
 import C from './C';
 import App from './App';
 import History from './History';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 export const makeMainRoutes = () => {
   return (
     <Router history={History} component={App}>
-      <div>
-        <Route path="/" render={(props) => <App {...props} />} />
-        <Route path="/a" render={(props) => <A {...props} />} />
-        <Route path="/b" render={(props) => <B {...props} />} />
-        <Route path="/c" render={(props) => <C {...props} />} />
+      <Route render={({ location }) => (
+      <div className="mainContainer">
+      <header className="App-header">
+        <h1 className="App-title">Mike Dreiling</h1>
+        <p>
+          Im just a dude looking to get right back into code.  But above all, I love
+          to create things.
+        </p>
+      </header>
+      <div className="logoBoxA">
+        <Link to="/a"><h2>A</h2></Link>
       </div>
+      <div className="logoBoxB">
+        <Link to="/b"><h2>B</h2></Link>
+      </div>
+      <div className="logoBoxC">
+        <Link to="/c"><h2>C</h2></Link>
+      </div>
+      <div className="logoBoxHome">
+        <Link to="/"><h2>Home</h2></Link>
+      </div>
+        <TransitionGroup>
+          <CSSTransition key={location.key} classNames="fade" timeout={300}>
+            <Switch location={location}>
+              <Route exact path="/" render={(props) => <App />} />
+              <Route exact path="/a" render={(props) => <A />} />
+              <Route exact path="/b" render={(props) => <B />} />
+              <Route exact path="/c" render={(props) => <C />} />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+      </div>
+      )}
+      />
     </Router>
   );
 }
