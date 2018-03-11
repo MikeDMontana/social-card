cb998bdc2909aa5afc8bc3ec788c74c9d26ee807
Mike Dreiling <mdarts@gmail.com>
2018-03-11 11:10:39 -0600

    redux working now integrate it into routerv4

--- src/A.js
+++ src/A.js
@@ -1,20 +1,57 @@
 import React, { Component } from 'react';
 import { createStore } from 'redux';
+import expect from 'expect';
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      if (state === 0) {
+        return state - 0;
+      } else {
+        return state - 1;
+        }
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter);
+store.dispatch({ type: 'DECREMENT' });
 
 class A extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      counterNum:0
+    }
   }
 
-  test = () => {
-    console.log("hello");
+  plusClick = () => {
+    store.dispatch({ type: 'INCREMENT' });
+    console.log(store.getState());
+    this.setState({
+      counterNum:store.getState()
+    });
+  }
+
+  minusClick = () => {
+    store.dispatch({ type: 'DECREMENT' });
+    console.log(store.getState());
+    this.setState({
+      counterNum:store.getState()
+    });
   }
 
   render() {
     return(
       <div className="aContainer">
         <h2>Component A</h2>
-        {this.test()}
+        <button onClick={this.plusClick.bind(this)}>+</button>
+        <button onClick={this.minusClick.bind(this)}>-</button>
+        <p>{this.state.counterNum}</p>
       </div>
     );
   }
